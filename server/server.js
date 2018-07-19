97a3723a6da96dbe9dead6ac6cd35fad8094f11f
lisalinlizhe <lisalizhe@gmail.com>
2018-07-18 21:35:15 -0700

    Add auth token

--- server/server.js
+++ server/server.js
@@ -102,11 +102,11 @@ app.post('/users', (req, res)=> {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then((user)=>{
-    console.log('!?!?!?');
-    res.send(user);
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth', token).send(user);
   }).catch((e)=>{
-    console.log('!!!!!!');
     res.status(400).send(e);
   })
 });
